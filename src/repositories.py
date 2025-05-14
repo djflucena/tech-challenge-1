34cf0d8adfa78316c92af84ee9beb8ac4e923e86
David Lucena <djflucena@sefaz.al.gov.br>
2025-05-14 00:23:41 +0000

    Criando RaspagemService e colocando no fluxo da funcionalidade

--- src/repositories.py
+++ src/repositories.py
@@ -2,61 +2,18 @@ class ProducaoRepository:
     """
         Repository para Produção de vinhos, sucos e derivados do Rio Grande do Sul.
     """
+    def __init__(self):
+        self.dados = None
+
     def get_por_ano(self, ano: int):
         """
             Retorna a produção de vinhos, sucos e derivados do Rio Grande do Sul por ano.
         """
-        return {
-            "Produto":[
-                    {
-                        "VINHO DE MESA":"217208604",
-                        "TIPOS":[
-                            {
-                            "Tinto":174224052
-                            },
-                            {
-                            "Branco":748400
-                            },
-                            {
-                            "Rosado":42236152
-                            }
-                        ]
-                    },
-                    {
-                        "VINHO FINO DE MESA (VINIFERA)":"23899346",
-                        "TIPOS":[
-                            {
-                            "Tinto":7591557
-                            },
-                            {
-                            "Branco":15562889
-                            },
-                            {
-                            "Rosado":744900
-                            }
-                        ]
-                    },
-                    {
-                        "SUCO":"1097771",
-                        "TIPOS":[
-                            {
-                            "Suco de uva integral":1097771
-                            },
-                            {
-                            "Suco de uva concentrado":0
-                            },
-                            {
-                            "Suco de uva adoçado":0
-                            },
-                            {
-                            "Suco de uva orgânico":0
-                            },
-                            {
-                            "Suco de uva reconstituído":0
-                            }
-                        ]
-                    }
-                ],
-            "Total":256370050
-        }
-    
+        return self.dados
+        
+    def salvar(self, dados):
+        """
+            Salva os dados de produção de vinhos, sucos e derivados do Rio Grande do Sul.
+        """
+        # Implementar lógica de salvamento no banco de dados ou outro armazenamento
+        self.dados = dados
--- src/services.py
+++ src/services.py
@@ -1,3 +1,4 @@
+from src.scrapping_service import RaspagemService
 from src.repositories import ProducaoRepository
 
 class ProducaoService:
@@ -6,10 +7,17 @@ class ProducaoService:
     """
     def __init__(self):
         self.producao_repository = ProducaoRepository()
+        self.scrapping_service = RaspagemService("https://www.example.com")
 
     def get_por_ano(self, ano: int):
         """
             Retorna a produção de vinhos, sucos e derivados do Rio Grande do Sul por ano.
         """
+        try:
+            self.scrapping_service.buscar_dados()
+            dados = self.scrapping_service.converter_dados()
+            self.producao_repository.salvar(dados)
+        except Exception as e:
+            print(f"Erro ao buscar dados: {e}")
         return self.producao_repository.get_por_ano(ano)
 
