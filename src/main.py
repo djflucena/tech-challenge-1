6ca9a4f2e0525cfe5f63de678ee331dea913037c
David Lucena <djflucena@sefaz.al.gov.br>
2025-05-14 00:43:39 +0000

    Criacao do filtro para o parâmetro ano

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
+from typing import Annotated
+from fastapi import Query
+
 from src.config import app
 from src.services import ProducaoService
+from src.filter_params import AnoFilterParams
 
 URL_BASE = "/vitivinicultura/api/v1"
 
 @app.get(URL_BASE+"/producao")
-async def producao(ano: int):
+async def producao(
+    ano: Annotated[
+        AnoFilterParams, 
+        Query(description="""Ano de produção dos vinhos, 
+              sucos e derivados do Rio Grande do Sul.""")
+        ]):
+    """
+        Endpoint para retornar a produção de vinhos, 
+        sucos e derivados do Rio Grande do Sul.
+    """
     return ProducaoService().get_por_ano(ano)
