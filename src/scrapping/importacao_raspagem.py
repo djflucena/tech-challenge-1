733f7eb72fc74e77f374c1e52fd2116d8e9e6748
David Lucena <djflucena@sefaz.al.gov.br>
2025-05-15 22:34:17 +0000

    Fazendo herancao de ImportacaoRaspagem com ComercioExteriorRaspagemAbstract

--- src/scrapping/importacao_raspagem.py
+++ src/scrapping/importacao_raspagem.py
@@ -1,9 +1,15 @@
-class ImportacaoRaspagem:
+from src.scrapping.comercio_exterior_raspagem import ComercioExteriorRaspagemAbstract
+
+class ImportacaoRaspagem(ComercioExteriorRaspagemAbstract):
     
     def __init__(self, ano: int, subopcao: str) -> None:
+        super().__init__()
         self.ano = ano
         self.subopcao = subopcao
         self.html = None
+
+    def construir_url(self):
+        pass
 
     def buscar_html(self) -> None:
         pass
