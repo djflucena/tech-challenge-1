c0e24e6a863b5bc7c51364bd52507877d3417b7d
David Lucena <djflucena@sefaz.al.gov.br>
2025-05-15 21:25:57 -0300

    ajustes nos services

--- src/services/producao_services.py
+++ src/services/producao_services.py
@@ -1,26 +1,32 @@
+"""
+Service para Produção de vinhos, sucos e derivados
+do Rio Grande do Sul.
+"""
+
 from src.raspagem.producao_raspagem import ProducaoRaspagem
 from src.repositories.producao_repository import ProducaoRepository
 
+
 class ProducaoService:
     """
-        Service para Produção de vinhos, sucos e derivados 
-        do Rio Grande do Sul.
+    Service para Produção de vinhos, sucos e derivados
+    do Rio Grande do Sul.
     """
+
     def __init__(self):
         self.producao_repository = ProducaoRepository()
 
     def get_por_ano(self, ano: int):
         """
-            Retorna a produção de vinhos, sucos e derivados 
-            do Rio Grande do Sul por ano.
+        Retorna a produção de vinhos, sucos e derivados
+        do Rio Grande do Sul por ano.
         """
         try:
             producao_raspagem = ProducaoRaspagem(ano)
             producao_raspagem.buscar_html()
             dados = producao_raspagem.parser_html()
-            
+
             self.producao_repository.salvar_ou_atualizar(dados, ano)
-        except Exception as e:
+        except Exception:
             print("Erro ao buscar dados")
         return self.producao_repository.get_por_ano(ano)
-
