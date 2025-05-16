9280dc7f0faea16555dd0fec2fa09096ea91ab83
David Lucena <djflucena@sefaz.al.gov.br>
2025-05-15 21:25:36 -0300

    ajustes nas classes de raspagem

--- src/raspagem/comercializacao_raspagem.py
+++ src/raspagem/comercializacao_raspagem.py
@@ -1,11 +1,14 @@
+"""Classe abstrata para raspagem de dados de comércio exterior: Importação e Exportação."""
 
 from src.raspagem.vitivinicultura_raspagem import VitiviniculturaRaspagem
 
+
 class ComercializacoRaspagem(VitiviniculturaRaspagem):
     """
-        Classe responsável por realizar a raspagem de dados 
-        da comercialização de vinhos, sucos e derivados do Rio Grande do Sul.
+    Classe responsável por realizar a raspagem de dados
+    da comercialização de vinhos, sucos e derivados do Rio Grande do Sul.
     """
+
     def __init__(self, ano: int):
         super().__init__()
         self.ano = ano
